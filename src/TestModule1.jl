ca27f53cc990c61866214a3748f6175155eb33ca
Alexandre Chen <napoleonalexandre@gmail.com>
2020-04-30 02:00:27 -0700

    create test.jl

--- src/TestModule1.jl
+++ src/TestModule1.jl
@@ -2,4 +2,26 @@ module TestModule1
 
 greet() = print("Hello World!")
 
-end # module
+export func1, print_nprocs
+
+using Distributed
+using SharedArrays
+using ProgressMeter
+using Interpolations
+
+function func1()
+    n = 2000
+    array = SharedArray{Float64}(n)
+    @sync @distributed for i = 1:n
+        arr[i] = i^2
+    end
+    res = sum(arr)
+    return res
+end
+
+function print_nprocs()
+    return nworkers()
+end
+
+end
+# end # module
